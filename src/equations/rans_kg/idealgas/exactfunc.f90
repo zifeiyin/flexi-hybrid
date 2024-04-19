25f07479de5ded874e9248613c7ad206096e5e46
Zifei Yin <yinzifei@sjtu.edu.cn>
2024-04-19 08:27:37 +0800

    positive source term if k is negative

--- src/equations/rans_kg/idealgas/exactfunc.f90
+++ src/equations/rans_kg/idealgas/exactfunc.f90
@@ -865,8 +865,10 @@ DO iElem=1,nElems
     END ASSOCIATE
 #endif
 
-    prodK (1,i,j,k,iElem) = 2. * muT * SijGradU                 
-    dissK (1,i,j,k,iElem) = Cmu * (UPrim(DENS) * kPos)**2 * invR
+
+    prodK (1,i,j,k,iElem) = 2. * muT * SijGradU     
+    dissK (1,i,j,k,iElem) = U(RHOK,i,j,k,iElem) / MAX( gPos**2, 1.e-10 ) ! hope to make negtive k come back           
+    !dissK (1,i,j,k,iElem) = Cmu * (UPrim(DENS) * kPos)**2 * invR
 
     prodG (1,i,j,k,iElem) = 0.5 * Comega2 / Cmu * UPrim(DENS) / MAX(gPos, 1.e-8) 
     dissG (1,i,j,k,iElem) = Comega1 * Cmu * UPrim(DENS) * gPos**3 * SijGradU * MIN( 100.*muTOrig/muS, 1.0)
