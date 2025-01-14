c739972b067275b9bd97006da5eb46480b9dbb8e
Shimushushushu <wangsijie@outlook.com>
2025-01-14 17:24:39 +0800

    Restart from rans_kg:TimeAvg

--- src/restart/restart.f90
+++ src/restart/restart.f90
@@ -175,6 +175,16 @@ ELSE
       CASE(TRIM(StrVarNamesPrim(7))) ! nuTilde
         RestartPrim(7) = iVar
 #endif /* EQNSYSNR == 3 */
+#if EQNSYSNR == 4
+      CASE(TRIM(StrVarNames(6)))     ! rhok
+        RestartCons(6) = iVar
+      CASE(TRIM(StrVarNames(7)))     ! rhog
+        RestartCons(7) = iVar
+      CASE(TRIM(StrVarNamesPrim(7))) ! tke
+        RestartPrim(7) = iVar
+      CASE(TRIM(StrVarNamesPrim(8))) ! omg
+        RestartPrim(8) = iVar
+#endif /* EQNSYSNR == 4 */
     END SELECT
   END DO
   ! Use conservative variables available
