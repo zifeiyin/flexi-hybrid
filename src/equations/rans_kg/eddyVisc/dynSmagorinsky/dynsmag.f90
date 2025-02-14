2fea26d3963bf7e4a58477c0f853985bb7eb6044
Shimushushushu <wangsijie@outlook.com>
2025-02-14 17:11:30 +0800

    fixed mus -> nus in adaptive des

--- src/equations/rans_kg/eddyVisc/dynSmagorinsky/dynsmag.f90
+++ src/equations/rans_kg/eddyVisc/dynSmagorinsky/dynsmag.f90
@@ -304,22 +304,22 @@ USE MOD_Equation_Vars,     ONLY: Cmu
 USE MOD_Mesh_Vars,         ONLY: nElems, Elem_hmx
 USE MOD_EddyVisc_Vars,     ONLY: CDES0, Cdes2
 USE MOD_DG_Vars,           ONLY: UPrim
-USE MOD_VISCOSITY 
+USE MOD_VISCOSITY
 IMPLICIT NONE
 !-----------------------------------------------------------------------------------------------------------------------------------
 !-----------------------------------------------------------------------------------------------------------------------------------
 ! LOCAL VARIABLES
 INTEGER             :: i,j,k,iElem
 REAL                :: eta, ratio, diss, Clim
-REAL                :: muS
+REAL                :: nuS
 !===================================================================================================================================
 
 ! Limit Dynamic Smagorinsky Coefficients
 DO iElem = 1,nElems
   DO k=0,PP_NZ; DO j=0,PP_N; DO i=0,PP_N
-    muS     = VISCOSITY_TEMPERATURE(UPrim(TEMP,i,j,k,iElem))
+    nuS     = VISCOSITY_TEMPERATURE(UPrim(TEMP,i,j,k,iElem)) / UPrim(DENS,i,j,k,iElem)
     diss    = MAX( UPrim(TKE,i,j,k,iElem) / UPrim(OMG,i,j,k,iElem)**2 , 1.e-16)
-    eta     = ( muS**3 / diss )**0.25  
+    eta     = ( nuS**3 / diss )**0.25
     ratio   = Elem_hmx(iElem) / eta
     Clim    = 0.5 * CDES0 * ( MAX( MIN( (ratio-23.0)/7.0, 1.0), 0.0) + MAX( MIN( (ratio-65.0)/25.0, 1.0), 0.0) )
     Cdes2(i,j,k,iElem) = MAX( Cdes2(i,j,k,iElem), Clim**2 )
