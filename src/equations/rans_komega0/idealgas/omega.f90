4c84500399f2aefdc43640c0ec15cd49ced9e689
Shimushu Wang <shimushu@sjtu.edu.cn>
2026-01-30 11:06:12 -0500

    Omega -> Omega0 function

--- src/equations/rans_komega0/idealgas/omega.f90
+++ src/equations/rans_komega0/idealgas/omega.f90
@@ -12,7 +12,17 @@ INTERFACE ComputeOmega
   MODULE PROCEDURE ComputeOmega
 END INTERFACE
 
+INTERFACE ComputeOmega2
+  MODULE PROCEDURE ComputeOmega2
+END INTERFACE
+
+INTERFACE SolveOmega0
+  MODULE PROCEDURE SolveOmega0
+END INTERFACE
+
 PUBLIC::ComputeOmega
+PUBLIC::ComputeOmega2
+PUBLIC::SolveOmega0
 
 CONTAINS
 
@@ -50,5 +60,81 @@ DO iElem=1,nElems
 END DO
 
 END SUBROUTINE ComputeOmega
+
+SUBROUTINE ComputeOmega2(UPrim, ywall, omega)
+USE MOD_Globals
+USE MOD_PreProc
+USE MOD_Equation_Vars, ONLY: Comega2, Cexp
+USE MOD_Viscosity
+
+REAL,INTENT(IN)  :: UPrim(PP_nVarPrim)
+REAL,INTENT(IN)  :: ywall
+REAL,INTENT(OUT) :: omega
+
+REAL    :: nuS, xi, omega0Pos
+
+nuS = VISCOSITY_TEMPERATURE(UPrim(TEMP)) / UPrim(DENS)
+omega0Pos = MAX(UPrim(OMG), 0.0)
+xi = omega0Pos * ywall**2 / nuS
+omega = omega0Pos + (6.0 / Comega2) * nuS / ywall**2 * EXP(-Cexp * xi)
+
+END SUBROUTINE ComputeOmega2
+
+SUBROUTINE SolveOmega0(UPrim, ywall, omega0)
+USE MOD_Globals
+USE MOD_PreProc
+USE MOD_Equation_Vars, ONLY: Comega2, Cexp
+USE MOD_Viscosity
+
+REAL,INTENT(IN)  :: UPrim(PP_nVarPrim)
+REAL,INTENT(IN)  :: ywall
+REAL,INTENT(OUT) :: omega0
+
+REAL    :: nuS, xi, omega, omega_asymptotic
+REAL    :: upper, lower, mid
+
+! UPrim = (rho, u, v, w, p, T, k, omega) instead of omega0
+
+nuS = VISCOSITY_TEMPERATURE(UPrim(TEMP)) / UPrim(DENS)
+
+omega_asymptotic = (6.0 / Comega2) * nuS / ywall**2
+
+IF (UPrim(OMG) .LE. omega_asymptotic) THEN
+  omega0 = 0.0
+  RETURN
+END IF
+
+! lower = 0.0
+! upper = UPrim(OMG)
+! DO
+!   mid = 0.5 * (lower + upper)
+!   xi = mid * ywall**2 / nuS
+!   omega = mid + omega_asymptotic * EXP(-Cexp * xi)
+!   IF ((ABS(omega - UPrim(OMG)) / UPrim(OMG) .LE. 1.0E-2).OR.&
+!       ((upper - lower) / mid .LE. 1.0E-2)) THEN
+!     EXIT
+!   ELSEIF (omega .GT. UPrim(OMG)) THEN
+!     upper = mid
+!   ELSE
+!     lower = mid
+!   END IF
+! END DO
+
+! omega0 = mid
+
+! omega0 = MAX(0.0, UPrim(OMG) - omega_asymptotic)
+omega0 = UPrim(OMG)
+DO
+  IF ((ABS(omega - UPrim(OMG)) / UPrim(OMG) .LE. 1.0E-2)) THEN
+    EXIT
+  END IF
+  xi = omega0 * ywall**2 / nuS
+  omega = omega0 + omega_asymptotic * EXP(-Cexp * xi)
+  omega0 = omega0 - &
+      (omega - UPrim(OMG)) / &
+      (1.0 - (6.0 / Comega2 * Cexp) * EXP(-Cexp * xi))
+END DO
+
+END SUBROUTINE SolveOmega0
 
 END MODULE MOD_Omega
